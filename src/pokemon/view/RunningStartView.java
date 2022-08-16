32044ed76451160423683a63beaa54b2a64ec180
Shin__Mini <gusals121234@gmail.com>
2022-08-16 10:04:16 +0900

    [UPDATE] startview에 희돈님이 추ㅏ하신 임시 임시 메뉴 추가 + sql 문장 정리해서 다시 올림

--- src/pokemon/view/RunningStartView.java
+++ src/pokemon/view/RunningStartView.java
@@ -1,70 +1,48 @@
 package pokemon.view;
 
-// import pokemon.controller.OwnerController;
-// import pokemon.controller.PokemonController;
-import pokemon.model.dto.OwnerDTO;
-import pokemon.model.dto.PokemonDTO;
+import java.lang.ModuleLayer.Controller;
+import java.util.Scanner;
+
 import pokemon.controller.PokemonController;
 
 public class RunningStartView {
+	static Scanner sc = new Scanner(System.in);
 	// 희돈님이 보내주신 파일로 수정 완료
 
 	public static void main(String[] args) {
-		printQuery();
+	
 	}
-
-	public static void printQuery() {
-//		System.out.println("**** 모든 포켓몬 검색 ****");
-//		PokemonController.getAllPokemons();
-		
-//		System.out.println("\\n**** 특정 포켓몬 검색 ****");
-//		PokemonController.getPokemon("Squirtle");
-		
-//		System.out.println("\n**** 특정 포켓몬 추가 ****");
-//		PokemonController.addPokemon(new PokemonDTO(16, "홍길동", 50, "물", 400, true));
-//		PokemonController.getPokemon("홍길동");
-		
-//		System.out.println("\n**** 특정 나이로 포켓몬 검색 ****");
-//		PokemonController.pokemonAge(50);
-		
-//		System.out.println("\n**** 특정 파워로 포켓몬 검색");
-//		PokemonController.pokemonPower(400);
-//		
-//		System.out.println("\n**** 특정 타입으로 포켓몬 검색");
-//		PokemonController.pokemonType("물");
-		
-//		System.out.println("\n**** 포켓몬 이름으로 파워 수정 ****");
-//		PokemonController.getPokemon("홍길동");
-//		PokemonController.updataPokemon("홍길동", 999);
-//		PokemonController.getPokemon("홍길동");
-
-		
-//		System.out.println("\\n**** 특정 포켓몬 삭제 ****");
-//		PokemonController.deletePokemon("홍길동");
-//		PokemonController.getPokemon("홍길동");
-
-		
-		
-		
-//		여기까지가 Owner 테스트 ************************************************************************
-//		System.out.println("**** 모든 소유자 검색 ****");
-//		OwnerController.getAllOwners();
-//		
-//		System.out.println("\n**** 특정 소유자 검색 ****");
-//		OwnerController.getOwner(10);
-		
-//		System.out.println("\n**** 소유자 신규 등록 ****");
-//		OwnerController.addOwner(new OwnerDTO(16, "홍길동", 99, "챌린저"));
-//		OwnerController.getOwner(16);
-		
-		
-//		System.out.println("\n**** 소유자 티어 수정 ****");
-//		OwnerController.updateOwner(16, "gold");
-//		OwnerController.getOwner(16);
-		
-//		System.out.println("\n**** 소유자 삭제 ****");
-//		OwnerController.deleteOwner(16);;
-//		OwnerController.getOwner(16);
-
+	
+	
+	// 임시 function 입니다. 파일 분리 예정.
+	public static void mainMenu() {
+		PokemonController cont; 
+		int select = 0;
+		while (true) {
+			try {
+				System.out.println("포켓몬 도감");
+				System.out.println("0.종료 1.포켓몬 도감 조회 2.포켓몬 조회 3. 포켓몬 마스터 조회");
+				select = sc.nextInt();
+				if (select == 0) {
+					RunningEndView.showMsg("프로그램을 종료합니다");
+					System.exit(0);
+				} else if (select > 3 || select < 0) {
+					RunningEndView.showMsg("정해진 범위 안에서 선택하세요");
+				} else if (select == 1) {
+					System.out.println("1.조회 2.추가 3.삭제 ...");
+					cont.pokemonBook(sc.nextInt());
+				} else if (select == 2) {
+					System.out.println("1.조회 2.추가 3.삭제 ...");
+					cont.pokemon(sc.nextInt());
+				} else if (select == 3) {
+					System.out.println("1.조회 2.추가 3.삭제 ...");
+					cont.owner(sc.nextInt());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				RunningEndView.showError("Exception 발생 프로그램을 종료합니다");
+				System.exit(0);
+			}
+		}
 	}
 }
